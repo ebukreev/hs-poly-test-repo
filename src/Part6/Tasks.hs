21e9ff014ed739bd6a076d8f7bba6868bc581a43
Evgeny Bukreev <ebukreev@ptsecurity.com>
2023-12-04 18:31:17 +0300

    implement Part6

--- src/Part6/Tasks.hs
+++ src/Part6/Tasks.hs
@@ -14,22 +14,32 @@ data SparseMatrix a = SparseMatrix {
 -- Определите класс типов "Матрица" с необходимыми (как вам кажется) операциями,
 -- которые нужны, чтобы реализовать функции, представленные ниже
 class Matrix mx where
+    eyeImpl :: Int -> mx
+    zeroImpl :: Int -> Int -> mx
 
 -- Определите экземпляры данного класса для:
 --  * числа (считается матрицей 1x1)
 --  * списка списков чисел
 --  * типа SparseMatrix, представленного выше
 instance Matrix Int where
+    eyeImpl _ = 1
+    zeroImpl _ _ = 0
+
 instance Matrix [[Int]] where
+    eyeImpl w = [[if i == j then 1 else 0 | j <- [1 .. w]] | i <- [1 .. w]]
+    zeroImpl w h = [[0 | j <- [1 .. w]] | i <- [1 .. h]]
+
 instance Matrix (SparseMatrix Int) where
+    eyeImpl w = SparseMatrix w w (fromList [((i, i), 1) | i <- [0 .. (w - 1)]])
+    zeroImpl w h = SparseMatrix w h mempty
 
 -- Реализуйте следующие функции
 -- Единичная матрица
 eye :: Matrix m => Int -> m
-eye w = notImplementedYet
+eye = eyeImpl
 -- Матрица, заполненная нулями
 zero :: Matrix m => Int -> Int -> m
-zero w h = notImplementedYet
+zero = zeroImpl
 -- Перемножение матриц
 multiplyMatrix :: Matrix m => m -> m -> m
 multiplyMatrix = notImplementedYet
